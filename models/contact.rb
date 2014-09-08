62a9316014efd85816e7f5cf0e7db51e2c737468
kms617 <krysia.codes@gmail.com>
2014-09-08 17:13:23 -0400

    core challenge completed

--- models/contact.rb
+++ models/contact.rb
@@ -1,13 +1,7 @@
-class Contact
-  attr_reader :first_name, :last_name, :phone_number
-
-  def initialize(attributes)
-    @first_name = attributes[:first_name]
-    @last_name = attributes[:last_name]
-    @phone_number = attributes[:phone_number]
-  end
+class Contact < ActiveRecord::Base
 
   def name
     [first_name, last_name].join(' ')
   end
+
 end
